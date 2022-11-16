cc49202d535e3346f582069ceba299a6ba559e08
samavm <88936498+samavm@users.noreply.github.com>
2022-11-16 22:25:53 +0400

    first task is completed.

---
+++ Allup/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Allup.DAL;
+using Allup.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Allup.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Setting> settings = _context.Settings.ToList();
+            ViewBag.Settings = settings;
+            return View();
+        }
+
+    }
+}
